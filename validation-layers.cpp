91bc834d0a695c5f94a242b3037b5f884094be46
Quentin Arehart <104207952+quentin-arehart@users.noreply.github.com>
2022-08-02 16:11:48 +0900

    Update validation-layers.cpp

--- validation-layers.cpp
+++ validation-layers.cpp
@@ -12,32 +12,32 @@ const uint32_t HEIGHT = 600; // values for window dimensions
 
 // Add two config variables to specify the layers to enable and whether to enable them or not
 const std::vector<const char*> validationLayers = {
-  "VK_LAYER_KHRONOS_validation"
-  }; 
+  	"VK_LAYER_KHRONOS_validation"
+	}; 
 
 #ifdef NDEBUG
-  const bool enableValidationLayers = false;
+  	const bool enableValidationLayers = false;
 #else
-  const bool enableValidationLayers = true;
+  	const bool enableValidationLayers = true;
 #endif
 
 
 // Application is wrapped into a class
 class HelloTriangleApplication {
 public:
-  void run() {
-    initWindow(); // initialize GLFW
-    initVulkan(); // initialize Vulkan
-    mainLoop(); // Keep window open until closed 
-    cleanup(); // clean up resources
+  	void run() {
+    	initWindow(); // initialize GLFW
+    	initVulkan(); // initialize Vulkan
+    	mainLoop(); // Keep window open until closed 
+    	cleanup(); // clean up resources
     }
   
 private:
-  VkInstance instance; // data member to hold the handle to the instance
+  	VkInstance instance; // data member to hold the handle to the instance
   
-  GLFWwindow* window; // reference the window
+  	GLFWwindow* window; // reference the window
   
-  void initWindow() {
+  	void initWindow() {
       // the first call in initWindow, tell it not to create an OpenGL context
       glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // initialize GLFW library
       // disable resizing windows
@@ -47,132 +47,133 @@ private:
     }
   
     void initVulkan() {
-/* The first thing needed to initialize the Vulkan library is to create an instance.
-    It is the connection between the application and the library, and needs specific details. */
-      vkcreateInstance(); 
+		/* The first thing needed to initialize the Vulkan library is to create an instance.
+		It is the connection between the application and the library, and needs specific details. */
+      	vkcreateInstance(); 
       
     }
   
     void mainLoop() {
-      while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-      }
+      	while(!glfwWindowShouldClose(window)) {
+        	glfwPollEvents();
+      	}
       
     }
   
- void cleanup() { // clean up resources by destroying window and terminating GLFW
-  vkDestroyInstance(instance, nullptr); 
+ 	void cleanup() { // clean up resources by destroying window and terminating GLFW
+  		vkDestroyInstance(instance, nullptr); 
   
-  glfwDestroyWindow(window);
+  		glfwDestroyWindow(window);
   
-  glfwTerminate();
+  		glfwTerminate();
       
     }
 
-void createInstance() { // Create an instance by filling in a struct with some info about the application
-  // Use validation layers function
-  if (enableValidationLayers && !checkValidationLayerSupport()) {
-    throw std::runtime_error("validation layers requested, but not available!");
-  }
+	void createInstance() { // Create an instance by filling in a struct with some info about the application
+  		// Use validation layers function
+  		if (enableValidationLayers && !checkValidationLayerSupport()) {
+   			throw std::runtime_error("validation layers requested, but not available!");
+  		}
   
   
-  VKApplicationInfo appInfo{}; // VkApplicationInfo is the struct
-  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  app.Info.pApplicationName = "Hello Triangle";
-  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.pEngineName = "No Engine";
-  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-  appInfo.apiVersion = VK_API_VERSION_1_0;
+  		VKApplicationInfo appInfo{}; // VkApplicationInfo is the struct
+  		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  		app.Info.pApplicationName = "Hello Triangle";
+  		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  		appInfo.pEngineName = "No Engine";
+  		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  		appInfo.apiVersion = VK_API_VERSION_1_0;
   
-  VkInstanceCreateInfo createInfo{};
-  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  createInfo.pApplicationInfo = &appInfo;
-  // Include validation names if they are enabled
-  if (enableValidationLayers) {
-    createInfo.enableLayerCount = static_cast<uint32_t>(validationLayers.size());
-    createInfo.ppEnableLayerNames = validationLayers.data();
-  } else {
-    createInfo.enabledLayerCount = 0;
-  }
+  		VkInstanceCreateInfo createInfo{};
+  		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  		createInfo.pApplicationInfo = &appInfo;
+  		// Include validation names if they are enabled
+  		if (enableValidationLayers) {
+    		createInfo.enableLayerCount = static_cast<uint32_t>(validationLayers.size());
+    		createInfo.ppEnableLayerNames = validationLayers.data();
+  		} else {
+    		createInfo.enabledLayerCount = 0;
+  		}
   
-  createInfo.enabledExtensionCount = glfwExtensionCount;
-  createInfo.ppEnabledExtensionNames = glfwExtension;
+  		createInfo.enabledExtensionCount = glfwExtensionCount;
+  		createInfo.ppEnabledExtensionNames = glfwExtension;
   
-  // Determine the global validation layers to enable
-  createInfo.enabledLayerCount = 0;
+  		// Determine the global validation layers to enable
+  		createInfo.enabledLayerCount = 0;
   
-  // Issue the vkCreateInstance call
-  VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+  		// Issue the vkCreateInstance call
+  		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
   
-  if(vkCreateInstance(&createInfo, nullptr, &instance != VK_SUCCESS)) {
-    throw std::runtime_error("failed to create instance!");
-  }
-  
- // Checking for extension support
- // Allocate an array to hold  the extension details. Request the number of extensions by leaving the
- // Latter parameter empty
- uint32_t extensionCount = 0;
- vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  		if(vkCreateInstance(&createInfo, nullptr, &instance != VK_SUCCESS)) {
+    		throw std::runtime_error("failed to create instance!");
+  		}
+ 	 
+ 		// Checking for extension support
+ 		// Allocate an array to hold  the extension details. Request the number of extensions by leaving the
+ 		// Latter parameter empty
+ 		uint32_t extensionCount = 0;
+ 		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
  
- // Allocate an array to hold the extension details (#include <vector>)
- std::vector<VkExtensionProperties> extensions(extensionCount);
+ 		// Allocate an array to hold the extension details (#include <vector>)
+ 		std::vector<VkExtensionProperties> extensions(extensionCount);
  
- // Query the extension details
- vkEnumerateInstanceExtensionProperties(nullptr, &glfwextensionCount, extensions.data());
+ 		// Query the extension details
+ 		vkEnumerateInstanceExtensionProperties(nullptr, &glfwextensionCount, extensions.data());
  
- // Each VkExtensionProperties struct contains the name and version of an extensions.
- // List them with a simple loop
- std::cout << "Available extensions:\n";
- for (const auto& extension : extensions) {
-  std::cout << '\t' << extension.extensionName << '\n';
- }
+ 		// Each VkExtensionProperties struct contains the name and version of an extensions.
+ 		// List them with a simple loop
+ 		std::cout << "Available extensions:\n";
+		
+		for (const auto& extension : extensions) {
+  		std::cout << '\t' << extension.extensionName << '\n';
+ 		}
  
-  // If the check was successful there should not be an error. 
+  		// If the check was successful there should not be an error. 
   
-  // Global extensions to interface with the window system
- std::vector<const char*> getRequiredExtensions() { 
-  uint32_t glfwExtensionCount = 0;
-  const char** glfwExtensions;
+  		// Global extensions to interface with the window system
+ 		std::vector<const char*> getRequiredExtensions() { 
+  		uint32_t glfwExtensionCount = 0;
+  		const char** glfwExtensions;
   
-  // Return the needed extensions and pass to the struct
-  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  		// Return the needed extensions and pass to the struct
+  		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
   
-  std::vector <const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+  		std::vector <const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
   
-  if (enableValidationLayers) {
-   extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-  }
+  		if (enableValidationLayers) {
+	   		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  		}
   
-  return extensions;
- }
+  		return extensions;
+ 	}
  
- // Add a new function called checkValidationLayerSupport that
-// checks if all requested layers are available. 
-bool checkValidationLayerSupport() {
-  uint32_t layerCount;
-  // List all available layers
-  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+ 	// Add a new function called checkValidationLayerSupport that
+	// checks if all requested layers are available. 
+	bool checkValidationLayerSupport() {
+  		uint32_t layerCount;
+  		// List all available layers
+  		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
   
-  std::vector<VkLayerProperties> availableLayers(layerCount);
-  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+  		std::vector<VkLayerProperties> availableLayers(layerCount);
+  		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
   
-  return false;
-}
+  		return false;
+	}
 
-// Check if all the layers in validationLayers exist in the availableLayers list
-for (const char* layerName : validationLayers) {
-  bool layerFound = false;
+	// Check if all the layers in validationLayers exist in the availableLayers list
+	for (const char* layerName : validationLayers) {
+  		bool layerFound = false;
   
-  for (const auto& layerProperties : availableLayers) {
-    if (strcmp(layerName, layerProperties.layerName) == 0) {
-      layerFound = true;
-      break;
-    }
+  		for (const auto& layerProperties : availableLayers) {
+    		if (strcmp(layerName, layerProperties.layerName) == 0) {
+      		layerFound = true;
+      		break;
+    	}
   }
   
-  if (!layerFound) {
-    return false;
-  }
+  	if (!layerFound) {
+    	return false;
+  	}
 }
 
 return true;
@@ -190,38 +191,38 @@ return true;
     throw std::runtime_error("failed to create instance!");
   }
   
- // Checking for extension support
- // Allocate an array to hold  the extension details. Request the number of extensions by leaving the
- // Latter parameter empty
- uint32_t extensionCount = 0;
- vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+ 	// Checking for extension support
+ 	// Allocate an array to hold  the extension details. Request the number of extensions by leaving the
+ 	// Latter parameter empty
+ 	uint32_t extensionCount = 0;
+ 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
  
- // Allocate an array to hold the extension details (#include <vector>)
- std::vector<VkExtensionProperties> extensions(extensionCount);
+ 	// Allocate an array to hold the extension details (#include <vector>)
+ 	std::vector<VkExtensionProperties> extensions(extensionCount);
  
- // Query the extension details
- vkEnumerateInstanceExtensionProperties(nullptr, &glfwextensionCount, extensions.data());
+ 	// Query the extension details
+ 	vkEnumerateInstanceExtensionProperties(nullptr, &glfwextensionCount, extensions.data());
  
- // Each VkExtensionProperties struct contains the name and version of an extensions.
- // List them with a simple loop
- std::cout << "Available extensions:\n";
- for (const auto& extension : extensions) {
-  std::cout << '\t' << extension.extensionName << '\n';
- }
+	// Each VkExtensionProperties struct contains the name and version of an extensions.
+ 	// List them with a simple loop
+ 	std::cout << "Available extensions:\n";
+ 	for (const auto& extension : extensions) {
+  		std::cout << '\t' << extension.extensionName << '\n';
+ 	}
   
 };
 
 
 
 int main() {
-  HelloTriangleApplication app;
+  	HelloTriangleApplication app;
   
-  try {
-    app.run();
-  } catch (const std::exception& e) {
-      std::cerr << e.what() << std::endl;
-      return EXIT_FAILURE;
-  }
+  	try {
+    	app.run();
+  	} catch (const std::exception& e) {
+      	std::cerr << e.what() << std::endl;
+      	return EXIT_FAILURE;
+  	}
   
-  return EXIT_SUCCESS;
+  	return EXIT_SUCCESS;
 }
